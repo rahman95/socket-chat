df0b125ca4471d8efd52c333b5fa0412008ac570
Rahman Younus <rahman_95@live.co.uk>
2017-10-29 17:50:48 +0000

    added fontAwesome to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,21 @@ module.exports = {
   entry: './src/client.js',
   module: {
     loaders: [
-        {
-            test: /\.scss$/,
-            loaders: ['style-loader', 'css-loader', 'sass-loader']
-        }
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/',    // where the fonts will go
+            publicPath: '/bin/'       // override the default path
+          }
+        }]
+      }
     ]
   },
   output: {
