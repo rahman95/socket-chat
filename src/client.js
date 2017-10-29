eb7a173e2cf6469d752e20f5309a3d6837fdb59f
Rahman Younus <rahman_95@live.co.uk>
2017-10-29 16:55:03 +0000

    added sass loader and bootstrap-sass

--- src/client.js
+++ src/client.js
@@ -1,6 +1,8 @@
 import 'jquery';
 import 'bootstrap';
 import io from 'socket.io-client';
+require('../src/client.scss');
+
 
 const socket = io('http://localhost:8080');
 socket.on('connected', function (data) {
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,14 @@ const path = require('path');
 
 module.exports = {
   entry: './src/client.js',
+  module: {
+    loaders: [
+        {
+            test: /\.scss$/,
+            loaders: ['style-loader', 'css-loader', 'sass-loader']
+        }
+    ]
+  },
   output: {
     path: path.resolve(__dirname, 'bin'),
     filename: 'client.bundle.js',
